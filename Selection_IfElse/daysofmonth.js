28a8e1f3f9f48e4293deebecae35f59b9d647d6e
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-04 00:19:52 +0530

    [kanak]Add . Added a program of day and month

---
+++ Selection_IfElse/daysofmonth.js
@@ -0,0 +1,9 @@
+const date=Number(process.argv[2])
+const month=Number(process.argv[3])
+if((month===3 && date>=20) || (month>3 && month<6)||(month===6 && date<=20)){
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+
