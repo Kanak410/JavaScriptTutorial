04f1320eb062585290a11262f3b12ca2c7305ae5
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-04 01:52:16 +0530

    [kanak]Add. Added digit to word converter program

---
+++ Selection_Switch/digitWord.js
@@ -0,0 +1,35 @@
+const digit =Number(process.argv[2]);
+switch(digit){
+    case 0:
+    console.log("Zero");
+    break;
+    case 1:
+        console.log("one");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break ;
+    case 4:
+        console.log("four");
+        break;
+    case 5:
+        console.log("five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+        case 8:
+            console.log("Eight");
+            break;
+        case 9:
+            console.log("Nine");
+            break;
+        default:
+            console.log("Enter single digit number ");
+
+    
+
+}
