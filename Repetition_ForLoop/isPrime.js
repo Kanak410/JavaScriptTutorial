3c53e04d38367ed621114695c0ffdb000d7fea0e
Kanak Rajput <rajputkanak078@gmail.com>
2025-03-04 12:00:38 +0530

    [kanak]Add. extended prime number program to print prime number

---
+++ Repetition_ForLoop/isPrime.js
@@ -0,0 +1,19 @@
+const num = Number(process.argv[2]); 
+let isPrime = true;
+
+if (num < 2) {
+    isPrime = false; 
+} else {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) { 
+            isPrime = false; 
+            break; 
+        }
+    }
+}
+
+if (isPrime) {
+    console.log(`${num} is a prime number.`);
+} else {
+    console.log(`${num} is not a prime number.`);
+}
